0c04037c464b7e1c49db7175b07db2531b4216d4
Miguel Martín <alu0101209777@ull.edu.es>
2020-12-14 22:57:53 +0000

    opens cam

--- python/main.py
+++ python/main.py
@@ -7,31 +7,26 @@ if not cap.isOpened():
     print('Unable to open cam')
     exit(0)
 
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
 
-# print ("W:", frame_width, "H:", frame_height)
-fourcc = cv.VideoWriter_fourcc('M','J','P','G')
-out = cv.VideoWriter('out.avi', fourcc, 20.0, 
-    (frame_width, frame_height))
 
 while True:
     ret, frame = cap.read()
     if not ret:
         print("Can't receive frame. Exiting...")
         exit(0)
+
+    frame = cv.flip(frame, 1)
     cv.imshow('Frame', frame)
 
-    out.write(frame)
 
     keyboard = cv.waitKey(1)
     if keyboard & 0xFF == ord('q'):
         break
 
 cap.release()
-out.release()
+
 cv.destroyAllWindows()
 
 
-# def record(output_file):
+
     
