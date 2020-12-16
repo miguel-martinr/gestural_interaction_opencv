072f348264b5bc20c2d9da39fbb04b08ede05b39
Miguel Martín <alu0101209777@ull.edu.es>
2020-12-16 00:11:01 +0000

    hasta convex hull

--- python/bg_substractor.py
+++ python/bg_substractor.py
@@ -1,23 +1,31 @@
-import cv2 as cv
+import cv2
+import numpy as np
 
-learning_rate = -1
-subst = cv.createBackgroundSubtractorMOG2(detectShadows = True)
+trackbar_def = 200
+trackbar_max = 1000
+learning_rate = trackbar_def / 10000
+subst = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
 is_trackbar_launched = False
-window_name = "Background Subst"
+learning_rate_window_name = "Learning Rate"
 
 def on_learning_rate_trackbar(val):
-    global learning_rate 
+    global learning_rate
     learning_rate = val / 10000
+    blank_background = np.zeros((50, 320, 3), np.uint8)
+    cv2.putText(blank_background, "Learning rate : " + str(learning_rate), (5, 25), 
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0))
+    cv2.imshow(learning_rate_window_name, blank_background)
 
 
 def launch_learning_rate_trackbar():
-    global window_name
+    global learning_rate_window_name
     position = (700, 20)
-    cv.namedWindow(window_name, cv.WINDOW_AUTOSIZE)
-    # cv.resizeWindow(window_name, 300, 30)
-    cv.moveWindow(window_name, position[0], position[1])
-    cv.createTrackbar("Learning rate * 10000", window_name, 200, 1000,
-        on_learning_rate_trackbar)
+    cv2.namedWindow(learning_rate_window_name, cv2.WINDOW_AUTOSIZE)
+    cv2.resizeWindow(learning_rate_window_name, 1, 50)
+    cv2.moveWindow(learning_rate_window_name, position[0], position[1])
+    cv2.createTrackbar("Learning rate * 10000", learning_rate_window_name, 
+        trackbar_def, 1000, on_learning_rate_trackbar)
+    on_learning_rate_trackbar(trackbar_def)
   
 # mat : px matrix
 def apply(mat):
--- python/main.py
+++ python/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-import cv2 as cv
+import cv2
 import bg_substractor as bg_subst
 
 learning_rate = -1
@@ -9,7 +9,7 @@ learning_rate = -1
 
 
 
-cap = cv.VideoCapture("http://192.168.1.51:4747/mjpegfeed?640x480")
+cap = cv2.VideoCapture("http://192.168.1.51:4747/mjpegfeed?640x480")
 
 
 
@@ -32,33 +32,45 @@ while True:
         print("Can't receive frame. Exiting...")
         exit(0)
 
-    frame = cv.flip(frame, 1)
+    frame = cv2.flip(frame, 1)
     roi = frame[up_left[1]:down_right[1], 
         up_left[0]:down_right[0], :]
 
     
-    cv.rectangle(frame, up_left, down_right, (255,0,0))
-    cv.imshow('Frame', frame)
+    cv2.rectangle(frame, up_left, down_right, (255,0,0))
+    cv2.imshow('Frame', frame)
     
     # Substracting background
     fg_mask = bg_subst.apply(roi)
 
     # Thresholding
-    thresh = cv.th
+    ret, thresh = cv2.threshold(fg_mask, 127, 255, cv2.THRESH_BINARY)
     
     # Finding contours
-    
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+    cv2.drawContours(thresh, contours, -1, (0, 255, 0), 3)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+    cv2.drawContours(roi, contours, -1, (0, 255, 0), 3)
+  
 
-    cv.imshow(bg_subst.window_name, fg_mask)
+    # Getting convex hull
+    if len(contours) > 0:
+        hull = cv2.convexHull(contours[0])
+        cv2.drawContours(roi, [hull], 0, (255, 0, 0), 3)
 
-    keyboard = cv.waitKey(1)
+
+    cv2.imshow("ROI", roi)
+    cv2.imshow("Thresh", thresh)
+    cv2.imshow("FG_Mask", fg_mask)
+
+    keyboard = cv2.waitKey(1)
     if keyboard & 0xFF == ord('q'):
         break
 
 
 
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
 
 
 
